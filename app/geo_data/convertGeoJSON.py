ab31529a71a41a489b82d00bd89be9d2dbb672e0
Tyler Wiseman <wisemantyr@gmail.com>
2020-06-13 18:20:05 -0400

    removed geo_data

--- app/geo_data/convertGeoJSON.py
+++
@@ -1,8 +0,0 @@
-import geopandas as gpd
-
-zipfile = r"zip://tl_2019_us_cd116.zip"
-districts_gdf = gpd.read_file(zipfile) #use geopandas to create geopandas dataframe from zip file
-
-districts_gdf.to_file("cd_116.geojson", driver='GeoJSON') #save dataframe as geoJSON
-
-#the geoJSON was loaded into Mapbox to create a tile layer. Styling for map done in Mapbox Studio.
--- buildDB.py
+++ buildDB.py
@@ -1,25 +1,12 @@
 import requests
 from config import key
 import pymongo
-import geopandas as gpd
-import geojson
 import json
 
 def build_mongo_db ():
     conn = 'mongodb://localhost:27017' # Create connection variable
     client = pymongo.MongoClient(conn)  # Pass connection to the pymongo instance.
     db = client.congress_db  # create/connect to db
-
-    #open geojson file created in convertGeoJSON script
-    geojson_path = r"geo_data/cd_116.geojson"
-    with open(geojson_path) as f:
-        geodata = geojson.load(f)
-    
-    features = geodata["features"] #get features array
-
-    #insert array into mongo collection
-    db.features.drop()
-    db.features.insert_many(features)
 
     #get json from votes URL
     votes_url = "https://api.propublica.org/congress/v1/house/votes/recent.json"
